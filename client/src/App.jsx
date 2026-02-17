56b8b9cbd1cbaabbb429a946897895085d475622
MarsalShyam <shyam12marsal@gmail.com>
2026-02-17 06:59:46 +0530

    created all the pages, set the routing and created left side of LoginPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Feed from './pages/Feed'
+import Login from './pages/Login'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Discover from './pages/Discover'
+import Connection from './pages/Connection'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
 
 const App = () => {
   return (
-    <div>
-      Hii ram
-      <p className='text-3xl font-bold'>How are your?</p>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Login/>}>
+        <Route index element={<Feed/>}/>
+        <Route path='messages' element={<Messages/>}/>
+        <Route path='messages/:userId' element={<ChatBox/>}/>
+        <Route path='connections' element={<Connection/>}/>
+        <Route path='discover' element={<Discover/>}/>
+        <Route path='profile' element={<Profile/>}/>
+        <Route path='profile/:profileId' element={<Profile/>}/>
+        <Route path='create-post' element={<CreatePost/>}/>
+      </Route>
+    </Routes>
+    </>
   )
 }
 
